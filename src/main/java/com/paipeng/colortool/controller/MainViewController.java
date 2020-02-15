b65a05faa70d94538516545b5e0275f46b90cd84
Pai Peng <p.peng@s2icode.com>
2020-02-15 21:02:18 +0800

    change color to modify image

--- src/main/java/com/paipeng/colortool/controller/MainViewController.java
+++ src/main/java/com/paipeng/colortool/controller/MainViewController.java
@@ -4,6 +4,7 @@ import com.paipeng.colortool.utils.CommonUtils;
 import com.paipeng.colortool.utils.ImageUtils;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -61,27 +62,44 @@ public class MainViewController implements Initializable {
     @FXML
     private TextField blueColorTextField;
 
+
+    @FXML
+    private Slider cyanColorSlider;
+    @FXML
+    private TextField cyanColorTextField;
+    @FXML
+    private Slider magentaColorSlider;
+    @FXML
+    private TextField magentaColorTextField;
+    @FXML
+    private Slider yellowColorSlider;
+    @FXML
+    private TextField yellowColorTextField;
+    @FXML
+    private Slider keyColorSlider;
+    @FXML
+    private TextField keyColorTextField;
+
+
     private String imagePath;
 
     private Color backgroundColor;
 
-    private Color brightColor;
-    private Color darkColor;
+    private Color brightColor = Color.WHITE;
+    private Color darkColor = Color.BLACK;
 
     private int selectedColorPixel;
 
     private BufferedImage bufferedImage;
 
+    private double[] cmyk;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         logger.info("initialize");
 
-        backgroundColorPicker.setOnAction(event -> {
-            backgroundColor = backgroundColorPicker.getValue();
-            //GraphicsContext gc = labelCanvas.getGraphicsContext2D();
-            //gc.setFill(backgroundColorPicker.getValue());
-            //gc.fillRect(0, 0, labelCanvas.getWidth(), labelCanvas.getHeight());
-        });
+        setRGBColorTextFields(brightColor);
+
 
         pixelColorPicker.setOnAction(event -> {
             logger.info(((ColorPicker)event.getTarget()).getValue().toString());
@@ -93,12 +111,9 @@ public class MainViewController implements Initializable {
                 darkColor = ((ColorPicker)event.getTarget()).getValue();
             }
 
-            redColorTextField.setText(String.valueOf((int)(((ColorPicker)event.getTarget()).getValue().getRed()*255)));
-            greenColorTextField.setText(String.valueOf((int)(((ColorPicker)event.getTarget()).getValue().getGreen()*255)));
-            blueColorTextField.setText(String.valueOf((int)(((ColorPicker)event.getTarget()).getValue().getBlue()*255)));
-            //GraphicsContext gc = labelCanvas.getGraphicsContext2D();
-            //gc.setFill(backgroundColorPicker.getValue());
-            //gc.fillRect(0, 0, labelCanvas.getWidth(), labelCanvas.getHeight());
+            setRGBColorTextFields(((ColorPicker)event.getTarget()).getValue());
+
+            changeColor(brightColor, darkColor);
         });
 
         FileChooser fileChooser = new FileChooser();
@@ -111,25 +126,7 @@ public class MainViewController implements Initializable {
 
             showImage(imagePath);
         });
-        /*
 
-        DropShadow ds = new DropShadow();
-        ds.setOffsetY(3.0);
-        ds.setOffsetX(3.0);
-        ds.setColor(Color.GRAY);
-        labelCanvas.setEffect(ds);
-
-
-        labelWidthTextField.setText("300");
-        labelWidthTextField.setOnKeyReleased(eventHandler);
-        labelHeightTextField.setText("600");
-        labelHeightTextField.setOnKeyReleased(eventHandler);
-
-
-        drawShapes();
-        imagePath = this.getClass().getResource("/images/timg.jpeg").getPath();
-        drawImage(imagePath);
-        */
 
         imagePath = this.getClass().getResource("/images/image_view_holder.png").getPath();
 
@@ -138,10 +135,16 @@ public class MainViewController implements Initializable {
         colorToggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
             public void changed(ObservableValue<? extends Toggle> ov,
                                 Toggle old_toggle, Toggle new_toggle) {
+                logger.info(colorToggleGroup.getSelectedToggle().getUserData().toString());
                 if (colorToggleGroup.getSelectedToggle() != null) {
                     try {
-                        logger.info(colorToggleGroup.getSelectedToggle().getUserData().toString());
                         selectedColorPixel = Integer.parseInt(colorToggleGroup.getSelectedToggle().getUserData().toString());
+
+                        if (selectedColorPixel == 0) {
+                            setRGBColorTextFields(brightColor);
+                        } else {
+                            setRGBColorTextFields(darkColor);
+                        }
                     } catch (NullPointerException e) {
                         logger.error(e.getMessage());
                     } catch (NumberFormatException e) {
@@ -150,6 +153,68 @@ public class MainViewController implements Initializable {
                 }
             }
         });
+
+
+
+        cyanColorSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                logger.info("cyan slider value: " + observable.getValue());
+                cyanColorTextField.setText(String.valueOf(observable.getValue()));
+
+            }
+        });
+        magentaColorSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                logger.info("magenta slider value: " + observable.getValue());
+                magentaColorTextField.setText(String.valueOf(observable.getValue()));
+            }
+        });
+        yellowColorSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                logger.info("yellow slider value: " + observable.getValue());
+                yellowColorTextField.setText(String.valueOf(observable.getValue()));
+            }
+        });
+        yellowColorSlider.setOnMouseReleased(sliderMouseReleaseEventHandler);
+        keyColorSlider.setOnMouseReleased(event -> {
+            logger.info("key slider value: " + ((Slider)event.getTarget()).getValue());
+
+                });
+        keyColorSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                logger.info("key slider value: " + observable.getValue());
+                keyColorTextField.setText(String.valueOf(observable.getValue()));
+            }
+        });
+
+
+        setCMYKColorTextFields(brightColor);
+    }
+
+
+    private void setRGBColorTextFields(Color color) {
+
+        redColorTextField.setText(String.valueOf((int)(color.getRed()*255)));
+        greenColorTextField.setText(String.valueOf((int)(color.getGreen()*255)));
+        blueColorTextField.setText(String.valueOf((int)(color.getBlue()*255)));
+        setCMYKColorTextFields(color);
+    }
+
+    private void setCMYKColorTextFields(Color color) {
+        cmyk = ImageUtils.convertRGBToCMYK(color);
+        cyanColorTextField.setText(String.valueOf(cmyk[0]));
+        magentaColorTextField.setText(String.valueOf(cmyk[1]));
+        yellowColorTextField.setText(String.valueOf(cmyk[2]));
+        keyColorTextField.setText(String.valueOf(cmyk[3]));
+
+        cyanColorSlider.setValue(cmyk[0]);
+        magentaColorSlider.setValue(cmyk[1]);
+        yellowColorSlider.setValue(cmyk[2]);
+        keyColorSlider.setValue(cmyk[3]);
     }
 
     private void showImage(String imagePath) {
@@ -167,8 +232,33 @@ public class MainViewController implements Initializable {
         }
     }
 
-    private void changeColor(Color color, int selectedColorPixel) {
+    private void changeColor(int selectedColorPixel, Color color) {
         BufferedImage bufferedImage2 = ImageUtils.changeBufferedImageWithColor(bufferedImage, color, selectedColorPixel);
+        imageView.setImage(ImageUtils.convertBufferedImageToImage(bufferedImage2));
+    }
+
+    private EventHandler sliderMouseReleaseEventHandler = new EventHandler() {
+        @Override
+        public void handle(Event event) {
+            logger.info("sliderMouseReleaseEventHandler slider value: " + ((Slider)event.getSource()).getValue());
+
+        }
+    };
+
+
+
+    private void changeColor(int selectedColorPixel, int red, int green, int blue) {
+        logger.info("changeColor " + red + " " + green + " " + blue);
+        BufferedImage bufferedImage2 = ImageUtils.changeBufferedImageWithColor(bufferedImage, selectedColorPixel, red, green, blue);
+        imageView.setImage(ImageUtils.convertBufferedImageToImage(bufferedImage2));
+    }
+
+
+    private void changeColor(Color brightColor, Color darkColor) {
+        logger.info("brightColor " + brightColor.toString());
+        logger.info("darkColor " + darkColor.toString());
+
+        BufferedImage bufferedImage2 = ImageUtils.changeBufferedImageWithColor(bufferedImage, brightColor, darkColor);
         imageView.setImage(ImageUtils.convertBufferedImageToImage(bufferedImage2));
     }
 
--- src/main/java/com/paipeng/colortool/utils/ImageUtils.java
+++ src/main/java/com/paipeng/colortool/utils/ImageUtils.java
@@ -5,7 +5,9 @@ import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
+
 
 public class ImageUtils {
     public static Image convertBufferedImageToImage(BufferedImage bf) {
@@ -23,8 +25,114 @@ public class ImageUtils {
         return wr;
     }
 
-    public static BufferedImage changeBufferedImageWithColor(BufferedImage bf, Color color, int selectedColorPixel) {
+    public static BufferedImage changeBufferedImageWithColor(BufferedImage bufferedImage, Color color, int selectedColorPixel) {
+        BufferedImage copyBufferedImage = copyImage(bufferedImage);
+        for (int x = 0; x < bufferedImage.getWidth(); x++) {
+            for (int y = 0; y < bufferedImage.getHeight(); y++) {
+                int pixelColor = bufferedImage.getRGB(x, y);
+                int rgb = ((((int) color.getRed() * 255) & 0x0ff) << 16) | ((((int) color.getGreen() * 255) & 0x0ff) << 8) | (((int) color.getBlue() * 255) & 0x0ff);
+                if (selectedColorPixel == 0) {
+                    if (pixelColor > 122) {
+                        copyBufferedImage.setRGB(x, y, rgb);
+                    }
+                } else {
+                    if (pixelColor <= 122) {
+                        copyBufferedImage.setRGB(x, y, rgb);
+                    }
+                }
+                bufferedImage.setRGB(x, y, bufferedImage.getRGB(x, y));
+            }
+        }
+        return copyBufferedImage;
+    }
 
-        return null;
+
+    public static BufferedImage changeBufferedImageWithColor(BufferedImage bufferedImage, Color brightColor, Color darkColor) {
+        BufferedImage copyBufferedImage = copyImage(bufferedImage);
+        for (int x = 0; x < bufferedImage.getWidth(); x++) {
+            for (int y = 0; y < bufferedImage.getHeight(); y++) {
+                int pixelColor = bufferedImage.getRGB(x, y);
+                int rgb = 0;
+                if (Color.rgb((pixelColor& 0xFFFFFF) >> 16, (pixelColor& 0xFFFF) >> 8, pixelColor& 0xFF).getBrightness() > 122.0/255) {
+                    rgb = (((int) (brightColor.getRed() * 255) & 0x0ff) << 16) | (((int) (brightColor.getGreen() * 255) & 0x0ff) << 8) | ((int) (brightColor.getBlue() * 255) & 0x0ff);
+                } else {
+                    rgb = (((int) (darkColor.getRed() * 255) & 0x0ff) << 16) | (((int) (darkColor.getGreen() * 255) & 0x0ff) << 8) | ((int) (darkColor.getBlue() * 255) & 0x0ff);
+                }
+                copyBufferedImage.setRGB(x, y, rgb);
+
+                bufferedImage.setRGB(x, y, bufferedImage.getRGB(x, y));
+            }
+        }
+        return copyBufferedImage;
+    }
+
+
+    public static BufferedImage changeBufferedImageWithColor(BufferedImage bufferedImage, int selectedColorPixel, int red, int green, int blue) {
+        BufferedImage copyBufferedImage = copyImage(bufferedImage);
+        for (int x = 0; x < bufferedImage.getWidth(); x++) {
+            for (int y = 0; y < bufferedImage.getHeight(); y++) {
+                int pixelColor = bufferedImage.getRGB(x, y);
+
+                if (selectedColorPixel == 0) {
+                    if (Color.rgb(pixelColor& 0xFFFFFF >> 16, pixelColor& 0xFFFF >> 8, pixelColor& 0xFF).getBrightness() > 122.0/255) {
+                        int rgb = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
+                        //int rgb = (blue << 16) | (green & 0x0ff << 8) | (red & 0x0ff);
+                        copyBufferedImage.setRGB(x, y, rgb);
+                    }
+                }
+                bufferedImage.setRGB(x, y, bufferedImage.getRGB(x, y));
+            }
+        }
+        return copyBufferedImage;
+    }
+
+    public static Color getBufferedImageBrightColor(BufferedImage bufferedImage) {
+        return Color.WHITE;
+    }
+
+
+    public static Color getBufferedImageDarkColor(BufferedImage bufferedImage) {
+        return Color.BLACK;
+    }
+
+
+    public static BufferedImage copyImage(BufferedImage source) {
+        BufferedImage b = new BufferedImage(source.getWidth(), source.getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics g = b.getGraphics();
+        g.drawImage(source, 0, 0, null);
+        g.dispose();
+        return b;
+    }
+
+    public static Color convertCMYKToRGB(float cyan, float magenta, float yellow, float key) {
+        // R = 255 × (1-C) × (1-K)
+        // G = 255 × (1-M) × (1-K)
+        // B = 255 × (1-Y) × (1-K)
+        double red = (1-cyan) * (1-key);
+        double green = (1-magenta) * (1-key);
+        double blue = (1-yellow) * (1-key);
+        return Color.color(red, green, blue);
+    }
+
+
+    public static double[] convertRGBToCMYK(Color color) {
+        double[] cmyk = new double[4];
+        double c, m, y, k;
+
+        k = Math.max(color.getRed(), color.getGreen());
+        k = 1 - Math.max(k, color.getBlue());
+
+        c = (1 - color.getRed() - k) / (1 - k);
+
+        m = (1 - color.getGreen() - k) / (1 - k);
+
+        y = (1 - color.getBlue() - k) / (1 - k);
+
+        cmyk[0] = c;
+        cmyk[1] = m;
+        cmyk[2] = y;
+        cmyk[3] = k;
+
+        return cmyk;
     }
 }
